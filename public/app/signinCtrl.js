0bd46e79930f65a75a9c2660d70b5c0bceddc1e4
mrwayneteng <mr.wayneteng@gmail.com>
2015-06-14 11:10:21 -0700

    signup screen

--- public/app/signinCtrl.js
+++ public/app/signinCtrl.js
@@ -4,5 +4,10 @@ myApp.controller('signinCtrl', function($scope, $location, UserService){
     $scope.signin = function() {
         UserService.authUser($scope.email, $scope.password);
         $scope.loggedin = true;
-    }
+    };
+    $scope.signup = function() {
+        UserService.createUser($scope.email,$scope.password);
+        $scope.loggin = true;
+
+    };
 });
