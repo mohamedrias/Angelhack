3ab3cd5e2552dba3580976518c0d939e05d52435
Kevin Nam <namkevi@gmail.com>
2015-06-14 11:49:25 -0700

    edited css for nav and mine

--- public/app/signupCtrl.js
+++ public/app/signupCtrl.js
@@ -1,11 +1,7 @@
 myApp.controller("signupCtrl", function($scope, notifier, $location, UserService){
     $scope.signup = function(){
         UserService.createUser($scope.email,$scope.password);
-<<<<<<< HEAD
         UserService.authUser($scope.email,$scope.password);
-=======
-        $scope.showmodal = true;
->>>>>>> 66e67240e106587883ee78a9884dbabccae1e400
     };
 
 });
